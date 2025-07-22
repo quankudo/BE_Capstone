b10a0c0f1fca38213e590abe74f000fb84ab0bc4
quankudo <quannguyen2004aa@gmail.com>
2025-07-22 23:10:10 +0700

    Init BE by NodeJs

---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+const db = require('../config/db');
+
+module.exports.getUsers = (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(results);
+  });
+};
